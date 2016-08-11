c098309aea12ea3d07d8e332902c2de16924c7c2
Joshua <joshling1919@gmail.com>
2016-08-10 18:01:43 -0700

    Added ShowExceptions class

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -8,24 +8,26 @@ class ShowExceptions
   end
 
   def call(env)
-    byebug
-    write_log(env)
-    app.call(env)
+    begin
+      app.call(env)
+    rescue RuntimeError => e
+      res = Rack::Response.new
+      res.status = 500
+      res['Content-type'] = "text/html"
+      res.write(render_exception(e))
+      res.finish
+    end
   end
 
   private
 
   def render_exception(e)
-    log_file = File.open('application.log', 'a')
-    log_text =  <<-LOG
-    TIME: #{Time.now}
-    IP: #{env['REMOTE_ADDR']}
-    PATH: #{env['REQUEST_PATH']}
-    ____________________________\n
-    LOG
-
-    log_file.write(log_text)
-    log_file.close
+    @exception = e
+    @backtrace = e.backtrace
+    match = /(.*?)\:/.match(@backtrace.join)
+    @source_code = File.binread(match[1]).to_s
+    file_content = File.read("lib/templates/rescue.html.erb")
+    content = ERB.new(file_content).result(binding)
   end
 
 end
